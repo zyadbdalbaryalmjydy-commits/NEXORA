53f83b1f586daa08fa7a813cac24b02fe10bdc68
zyadbdalbaryalmjydy-commits <zyadbdalbaryalmjydy@gmail.com>
2026-04-18 17:18:15 +0300

    Update deleteProject.js

--- api/deleteProject.js
+++ api/deleteProject.js
@@ -1,19 +1,51 @@
-let projects = [];
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, deleteDoc, doc } from "firebase/firestore";
+
+const firebaseConfig = {
+
+apiKey: process.env.FIREBASE_API_KEY,
+authDomain: "nexora-1c66b.firebaseapp.com",
+projectId: "nexora-1c66b",
+
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export default async function handler(req,res){
 
-const { project } =
-req.body;
+try {
 
-projects =
-projects.filter(
-p=>p!==project
-);
+const { project } = req.body;
+
+const snapshot = await getDocs(collection(db,"projects"));
+
+snapshot.forEach(async (d)=>{
+
+if(d.data().project === project){
+
+await deleteDoc(doc(db,"projects",d.id));
+
+}
+
+});
 
 res.status(200).json({
 
 success:true
 
 });
+
+}
+
+catch(err){
+
+res.status(500).json({
+
+error:"delete failed"
+
+});
+
+}
 
 }
