19027272c792b5baf6e3712e22fc029f822170de
zyadbdalbaryalmjydy-commits <zyadbdalbaryalmjydy@gmail.com>
2026-04-18 17:16:53 +0300

    Integrate Firebase to fetch projects from Firestore

--- api/getProjects.js
+++ api/getProjects.js
@@ -1,11 +1,43 @@
-let projects = [];
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+const firebaseConfig = {
+
+apiKey: process.env.FIREBASE_API_KEY,
+authDomain: "nexora-1c66b.firebaseapp.com",
+projectId: "nexora-1c66b",
+
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export default async function handler(req,res){
 
-res.status(200).json({
+try {
 
-projects
+const snapshot = await getDocs(collection(db,"projects"));
+
+const projects = [];
+
+snapshot.forEach(doc=>{
+
+projects.push(doc.data());
 
 });
+
+res.status(200).json({ projects });
+
+}
+
+catch(err){
+
+res.status(500).json({
+
+error:"fetch failed"
+
+});
+
+}
 
 }
