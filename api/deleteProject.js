3d49703e02531009c7cbfd64387ecbb665e05ca6
zyadbdalbaryalmjydy-commits <zyadbdalbaryalmjydy@gmail.com>
2026-04-19 16:31:12 +0300

    Refactor deleteProject.js to simplify handler
    Removed Firebase initialization and document deletion logic.

--- api/deleteProject.js
+++ api/deleteProject.js
@@ -1,51 +1,9 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, deleteDoc, doc } from "firebase/firestore";
-
-const firebaseConfig = {
-
-apiKey: process.env.FIREBASE_API_KEY,
-authDomain: "nexora-1c66b.firebaseapp.com",
-projectId: "nexora-1c66b",
-
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
 export default async function handler(req,res){
-
-try {
-
-const { project } = req.body;
-
-const snapshot = await getDocs(collection(db,"projects"));
-
-snapshot.forEach(async (d)=>{
-
-if(d.data().project === project){
-
-await deleteDoc(doc(db,"projects",d.id));
-
-}
-
-});
 
 res.status(200).json({
 
 success:true
 
 });
-
-}
-
-catch(err){
-
-res.status(500).json({
-
-error:"delete failed"
-
-});
-
-}
 
 }
