6c604309c344bfd59b6b4b04f264741051a8462b
zyadbdalbaryalmjydy-commits <zyadbdalbaryalmjydy@gmail.com>
2026-04-28 16:41:16 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,41 +1,18 @@
-import { initializeApp }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import {
-getAuth
-}
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-import {
-getDatabase
-}
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-
-apiKey: "AIzaSyBYEIxaoS1RbUCxqgj5g5rbpKZe1ddd80o",
-
-authDomain: "nexora-1c66b.firebaseapp.com",
-
-databaseURL: "https://nexora-1c66b-default-rtdb.firebaseio.com",
-
-projectId: "nexora-1c66b",
-
-storageBucket: "nexora-1c66b.firebasestorage.app",
-
-messagingSenderId: "964700323381",
-
-appId: "1:964700323381:web:5826dd261cb062f401fb7c"
-
+  apiKey: "AIzaSyBYEIxaoS1RbUCxqgj5g5rbpKZe1ddd80o",
+  authDomain: "nexora-1c66b.firebaseapp.com",
+  databaseURL: "https://nexora-1c66b-default-rtdb.firebaseio.com",
+  projectId: "nexora-1c66b",
+  storageBucket: "nexora-1c66b.firebasestorage.app",
+  messagingSenderId: "964700323381",
+  appId: "1:964700323381:web:5826dd261cb062f401fb7c"
 };
 
+const app = initializeApp(firebaseConfig);
 
-const app =
-initializeApp(firebaseConfig);
-
-export const auth =
-getAuth(app);
-
-export const db =
-getDatabase(app);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
