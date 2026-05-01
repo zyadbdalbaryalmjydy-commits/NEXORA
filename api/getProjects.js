e8390ee18655052932db0a3c9e13d9433f3fff9a
zyadbdalbaryalmjydy-commits <zyadbdalbaryalmjydy@gmail.com>
2026-05-01 17:21:36 +0300

    Refactor getProjects.js to simplify Firestore access

--- api/getProjects.js
+++ api/getProjects.js
@@ -1,70 +1,16 @@
-import { initializeApp } from "firebase/app";
-
-import {
-getFirestore,
-collection,
-getDocs
-} from "firebase/firestore";
-
-let app;
-
-if (!global.firebaseApp) {
-
-global.firebaseApp =
-initializeApp({
-
-apiKey: process.env.FIREBASE_API_KEY,
-
-authDomain:
-"nexora-1c66b.firebaseapp.com",
-
-projectId:
-"nexora-1c66b"
-
-});
-
-}
-
-app = global.firebaseApp;
-
-const db =
-getFirestore(app);
+import { db } from "../firebase.js";
+import { collection, getDocs } from "firebase/firestore";
 
 export default async function handler(req,res){
 
-try{
+const snapshot = await getDocs(collection(db,"projects"));
 
-const snapshot =
-await getDocs(
-collection(db,"projects")
-);
-
-const projects=[];
+let projects=[];
 
 snapshot.forEach(doc=>{
-
-projects.push(doc.data());
-
+  projects.push(doc.data());
 });
 
-res.status(200).json({
-
-projects
-
-});
-
-}
-
-catch(error){
-
-console.log(error);
-
-res.status(500).json({
-
-error:"Get failed"
-
-});
-
-}
+res.status(200).json({projects});
 
 }
