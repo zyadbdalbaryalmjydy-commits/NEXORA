46458e55a3c23e1d1beadc29680880ab39b1eca5
zyadbdalbaryalmjydy-commits <zyadbdalbaryalmjydy@gmail.com>
2026-05-02 16:48:25 +0300

    Update logo.js

--- api/logo.js
+++ api/logo.js
@@ -2,23 +2,23 @@ export default async function handler(req,res){
 
 const { idea } = JSON.parse(req.body);
 
-const response = await fetch("https://api.openai.com/v1/images/generations",{
-  method:"POST",
-  headers:{
-    "Authorization":`Bearer ${process.env.OPENAI_API_KEY}`,
-    "Content-Type":"application/json"
-  },
-  body:JSON.stringify({
-    model:"gpt-image-1",
-    prompt:`modern startup logo for ${idea}, minimal, clean`,
-    size:"512x512"
-  })
+const response=await fetch("https://api.openai.com/v1/images/generations",{
+method:"POST",
+headers:{
+"Authorization":`Bearer ${process.env.OPENAI_API_KEY}`,
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+model:"gpt-image-1",
+prompt:`modern logo for ${idea}`,
+size:"512x512"
+})
 });
 
-const data = await response.json();
+const data=await response.json();
 
 res.status(200).json({
-  url:data.data[0].url
+url:data.data[0].url
 });
 
 }
