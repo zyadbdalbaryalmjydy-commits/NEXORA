5914bba358b5a24d713204f26f542027591395e3
zyadbdalbaryalmjydy-commits <zyadbdalbaryalmjydy@gmail.com>
2026-05-02 16:49:43 +0300

    Update getProjects.js

--- api/getProjects.js
+++ api/getProjects.js
@@ -1,5 +1,13 @@
-import { db } from "../firebase.js";
-import { collection, getDocs } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+const app = initializeApp({
+apiKey: process.env.FIREBASE_API_KEY,
+authDomain:"nexora-1c66b.firebaseapp.com",
+projectId:"nexora-1c66b"
+});
+
+const db = getFirestore(app);
 
 export default async function handler(req,res){
 
@@ -8,9 +16,8 @@ const snapshot = await getDocs(collection(db,"projects"));
 let projects=[];
 
 snapshot.forEach(doc=>{
-  projects.push(doc.data());
+projects.push(doc.data());
 });
 
 res.status(200).json({projects});
-
 }
